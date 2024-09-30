6dd0a70d211c360c8714fcc43f0cca4f55cadb6c
Violet Caulfield <violet@cosmonautical.cloud>
2024-09-30 15:56:45 -0500

    Update list filter
    add some more packages
    
    setup desktop-session template

--- roles/nixos/filter_plugins/nix.py
+++ roles/nixos/filter_plugins/nix.py
@@ -5,6 +5,8 @@ class FilterModule(object):
             'nix_list': self.filter_list
         }
 
-    def filter_list(self, list):
+    def filter_list(self, list: list):
         nix_list_string = '[' + " ".join(list) + '];'
+
+        # list will have str and dict in it
         return nix_list_string
